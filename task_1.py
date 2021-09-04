3bfd596ee639a4becc77db77c8370f47ecdae2e4
Illia Loginov <ilyaloginovua@gmail.com>
2021-09-05 01:12:40 +0300

    Lab 1

---
+++ task_1.py
@@ -0,0 +1,69 @@
+class Payment:
+    def __init__(self, amount):
+        self.amount = amount
+
+
+class Cash(Payment):
+    def __init__(self, amount, currency):
+        super().__init__(amount)
+        self.currency = currency
+
+
+class Check(Payment):
+    def __init__(self, amount, name, bank_id):
+        super().__init__(amount)
+        self.name = name
+        self.bank_id = bank_id
+
+    def authorized(self):
+        return True
+
+
+class Credit(Payment):
+    def __init__(self, amount, number, credit_type, expiration_date):
+        super().__init__(amount)
+        self.number = number
+        self.type = credit_type
+        self.expiration_date = expiration_date
+
+    def authorized(self):
+        return True
+
+
+class Item:
+    def __init__(self, weight, description):
+        self.weight = weight
+        self.description = description
+
+    def get_tax(self):
+        return (self.weight / 200) * 0.4
+
+    def get_price(self):
+        return self.weight * 60
+
+
+class OrderDetail:
+    def __init__(self, quantity, tax_status):
+        self.quantity = quantity
+        self.tax_status = tax_status
+
+
+class Order:
+    def __init__(self, date, status, quantity, tax_status, item):
+        self.date = date
+        self.status = status
+        self.order_detail = OrderDetail(quantity, tax_status)
+        self.item = item
+
+    def calc_tax(self):
+        return Payment(self.item.get_tax() * self.item.get_price())
+
+    def calc_total(self):
+        return Payment((self.item.get_tax() + 1) * self.item.get_price())
+
+
+class Customer:
+    def __init__(self, name, address):
+        self.name = name
+        self.address = address
+        self.orders = []
---
+++ task_2.py
@@ -0,0 +1,107 @@
+import math
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+
+class Shape:
+    pass
+
+
+class Polygon(Shape):
+    def __init__(self, points):
+        self.points = points
+
+    def get_area(self):
+        area = 0
+        for i in range(len(self.points) - 1):
+            area += (self.points[i + 1].x - self.points[i].x) * (self.points[i + 1].y + self.points[i].y) * 0.5
+        area += (self.points[0].x - self.points[len(self.points) - 1].x) * (self.points[0].y + self.points[len(self.points) - 1].y) * 0.5
+        return area
+
+    def get_perimeter(self):
+        perimeter = 0
+        for i in range(len(self.points) - 1):
+            perimeter += math.sqrt((self.points[i + 1].x - self.points[i].x) * (self.points[i + 1].x - self.points[i].x) + (self.points[i + 1].y - self.points[i].y) * (self.points[i + 1].y - self.points[i].y))
+        perimeter += math.sqrt((self.points[0].x - self.points[len(self.points) - 1].x) * (self.points[0].x - self.points[len(self.points) - 1].x) + (self.points[0].y - self.points[len(self.points) - 1].y) * (self.points[0].y - self.points[len(self.points) - 1].y))
+        return perimeter
+
+
+class Triangle(Polygon):
+    def __init__(self, points):
+        super().__init__(points[0:3])
+
+
+class Rectangle(Polygon):
+    def __init__(self, points):
+        super().__init__(points[0:4])
+
+    def get_bottom_left_corner(self):
+        blc = self.points[0]
+        for point in self.points:
+            if point.x <= blc.x and point.y <= blc.y:
+                blc = point
+        return blc
+
+    def get_top_right_corner(self):
+        trc = self.points[0]
+        for point in self.points:
+            if point.x >= trc.x and point.y >= trc.y:
+                trc = point
+        return trc
+
+    def intersection_area(self, second_rectangle):
+        r1 = [self.get_bottom_left_corner(), self.get_top_right_corner()]
+        r2 = [second_rectangle.get_bottom_left_corner(), second_rectangle.get_top_right_corner()]
+
+        if r1[0].x <= r2[0].x <= r1[1].x:
+            left = r2[0].x
+        elif r2[0].x <= r1[0].x <= r2[1].x:
+            left = r1[0].x
+        else:
+            return 0
+
+        if r1[0].x <= r2[1].x <= r1[1].x:
+            right = r2[1].x
+        elif r2[0].x <= r1[1].x <= r2[1].x:
+            right = r1[1].x
+        else:
+            return 0
+
+        if r1[0].y <= r2[0].y <= r1[1].y:
+            bottom = r2[0].y
+        elif r2[0].y <= r1[0].y <= r2[1].y:
+            bottom = r1[0].y
+        else:
+            return 0
+
+        if r1[0].y <= r2[1].y <= r1[1].y:
+            top = r2[1].y
+        elif r2[0].y <= r1[1].y <= r2[1].y:
+            top = r1[1].y
+        else:
+            return 0
+
+        return (right - left) * (top - bottom)
+
+
+points_1 = [
+    Point(1, 1),
+    Point(1, 8),
+    Point(4, 8),
+    Point(4, 1)
+]
+rect_1 = Rectangle(points_1)
+
+points_2 = [
+    Point(2, 2),
+    Point(2, 10),
+    Point(6, 10),
+    Point(6, 2)
+]
+rect_2 = Rectangle(points_2)
+
+print(rect_1.intersection_area(rect_2))
+print(rect_2.intersection_area(rect_1))
